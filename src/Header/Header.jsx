cf947f4e1b36664ada681fe3dacf7e27dbd0d959
Ayodimeji-stacks <149108938+Ayodimeji-stacks@users.noreply.github.com>
2024-03-20 16:55:53 +0000

    first attempt at the toggle state

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,4 +1,9 @@
 import Link from "next/link";
+import { useState } from "react";
+export default function state () {
+  const onOffSwitch =
+}
+
 
 export default function Header(props) {
   return (
