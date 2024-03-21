f58f362647b3a928ad31a34d2740907a79bbbd6c
Stephen <145766101+INFINITY00users.noreplygithub.com>
2024-03-21 10:22:38 +0000

    plan added for click event

--- src/Clicker/Clicker.jsx
+++ src/Clicker/Clicker.jsx
@@ -1,9 +1,11 @@
+// Clicker.jsx
 import React from "react";
 
 export default function Clicker() {
   function handleClick() {
     console.log("I clicked the button");
   }
+
   return (
     <div>
       <p>Click the button</p>
--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,4 +1,17 @@
 import Link from "next/link";
+import "react";
+import "Header.css";
+
+// As a User I should be able to click on the menu button and the menu should then appear
+// As a User I should be able to click a close button and the menu should close
+
+// Make some toggle state
+// Make the state a boolean
+// Make the state false to begin with
+
+// Create a click handler function
+// Call the handler when the button is clicked
+// We are going to update state in that function
 
 export default function Header(props) {
   return (
