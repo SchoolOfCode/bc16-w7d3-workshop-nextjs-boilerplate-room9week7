488423230c0f9bdb2d50b27da69a9ef7790bd6dc
rikharthulee <rikharthulee@gmail.com>
2024-03-21 11:03:54 +0000

    Menu working!

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,13 +1,64 @@
-import Link from "next/link";
+"use client";
+
+import { useState, useEffect } from "react";
 import "./Header.css";
 
+import Link from "next/link";
+
+// When I click the on the menu icon and the menu should then appear
+// When I click the on the X icon the menu should disapear
+
+// Make some toggle state
+// Make the state a boolean
+// Make the state false to begin with
+
+// check our work
+
+// Create a click handler function
+// Call the handler when the open button is clicked
+// Call the handler when the closed button is clicked
+// The state should be updated
+
+// Conditional rendering of the menu
+// If state = false - menu is not rendered
+// If state = true - menu is rendered
+
+// Add some styling
+
+// As a developer I want to log every time a customer opens the menu
+
+// Import useEffect
+// write my useEffect
+// console.log("Menu Opened")
+// run the useEffect when menuToggle changes
+// check what the state actually is before we log (only run when true)
+
 export default function Header(props) {
+  const [menuToggle, setMenuToggle] = useState(false);
+
+  useEffect(() => {
+    if (menuToggle) {
+      console.log("menuOpenCount");
+    }
+  }, [menuToggle]);
+
+  function handleClick() {
+    setMenuToggle(!menuToggle);
+  }
+
   return (
     <>
       <header className="header">
         <h3>🔥Fireplace Palace</h3>
         <p>
-          <Link href="/">Home</Link>
+          <button onClick={handleClick}>
+            <Link href="/">Home</Link>
+          </button>
+          {menuToggle ? (
+            <div className="mobile-menu">
+              <button onClick={handleClick}>Close Menu</button>
+            </div>
+          ) : null}
         </p>
         <p>
           <Link href="/founders">Founders</Link>
